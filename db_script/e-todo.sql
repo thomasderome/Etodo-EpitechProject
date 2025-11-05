afbe3554c09d96606f33d012d84e3d5edbfd3f49
Trolldu47 <mrtrolldu47@gmail.com>
2025-11-05 13:20:50 +0100

    fix command start db

--- db_script/e-todo.sql
+++ db_script/e-todo.sql
@@ -1,18 +1,19 @@
-CREATE TABLE users (
+CREATE TABLE user (
     id varchar(36) PRIMARY KEY NOT NULL,
     email varchar(255) NOT NULL,
     password varchar(255) NOT NULL,
     name varchar(100) NOT NULL,
     firstname varchar(100) NOT NULL,
-    created_at DATE DEFAULT CURRENT_DATE
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE todo (
-    id PRIMARY KEY AUTOINCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title varchar(255) NOT NULL,
     description TEXT NOT NULL,
-    created_at DATE DEFAULT CURRENT_DATE,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     due_time DATE NOT NULL,
-    status DEFAULT 'not started' CHECK(status IN ('todo', 'in progress', 'done')),
+    status ENUM('todo', 'in progress', 'done') NOT NULL DEFAULT 'todo',
+    user_id VARCHAR(36) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
