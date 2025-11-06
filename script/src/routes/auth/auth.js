98a2df0ddd4a7c51a917a13304147f9518ff69ce
Trolldu47 <mrtrolldu47@gmail.com>
2025-11-06 09:19:34 +0100

    add system login

--- script/src/routes/auth/auth.js
+++ script/src/routes/auth/auth.js
@@ -1,5 +1,32 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const { pool } = require("../../config/db.js");
+
 const router = express.Router();
+
+router.post("/temp", async (req, res) => {
+    const password = await bcrypt.hash(req.body.password, 12);
+
+    await pool.query('UPDATE user SET password=?', [password])
+    res.send(password)
+})
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || typeof email !== "string"
+        || !password || typeof password !== "string") throw new TypeError();
+
+    const [result, fields] = await pool.query('SELECT password FROM user WHERE email=?', [email]);
+    if (result.length === 0) res.status(401).send({"msg": "Invalid Credentials"});
+    else {
+        const hashedPassword = await bcrypt.hash(result[0].password, 12);
+        const comparePassword = await bcrypt.compare(password, hashedPassword);
+
+        if (comparePassword) {
+            res.send('login');
+        } else res.status(401).send({"msg": "Invalid Credentials"});
+    }
+})
 
 module.exports = router;
