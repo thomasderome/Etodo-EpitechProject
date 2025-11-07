f3f164e0dd4cd66d4ce79cf9ec81ff9f1d8e1cdb
Trolldu47 <mrtrolldu47@gmail.com>
2025-11-07 11:39:52 +0100

    add omponent separate for user and in progress render the todo in sidebar

--- frontend/pages/todo/todo.tsx
+++ frontend/pages/todo/todo.tsx
@@ -75,21 +75,28 @@ import {
     AvatarImage,
 } from '@/components/ui/avatar';
 import { useIsMobile } from '@/hooks/use-mobile';
+import {Span} from "next/dist/server/lib/trace/tracer";
 
-
-export default function() {
-    return (<SideBarElement></SideBarElement>)
-}
+import { User_label } from "@/pages/todo/User_label";
 
 const data = {
     "user": {
         "name": "cxw",
         "email": "thomas.derome@epitech.eu",
         "avatar": "https://devforum-uploads.s3.dualstack.us-east-2.amazonaws.com/uploads/original/4X/1/b/8/1b85b9cd9cb10e440991a5f640b7312f7507370e.png",
-    }
+    },
+    "todo": [
+        {"cotegory": true, "title": "test", child: [
+                {"name": "test1"},
+                {"name": "test2"},
+                {"name": "test3"}
+            ]
+        },
+        {"cotegory": false, "title": "test_single"}
+    ]
 }
 
-export const SideBarElement = () => {
+export default function Todo_page() {
     const isMobile = useIsMobile();
 
     return (
@@ -100,27 +107,63 @@ export const SideBarElement = () => {
                     <SidebarMenuItem>
                         <SidebarMenu>
                             <SidebarMenuItem>
-                                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                                    <Avatar className="h-10 w-10 rounded-lg">
-                                        <AvatarImage src={data.user.avatar} alt={data.user.name}/>
-                                        <AvatarFallback>{data.user.name.slice(0, 2).toUpperCase()}</AvatarFallback>
-                                    </Avatar>
-                                    <div className="grid flex-1 text-left text-base leading-tight">
-                                        <span className="truncate font-bold">{data.user.name}</span>
-                                        <span className="truncate text-xs font-semibold">{data.user.email}</span>
-                                    </div>
-                                </div>
                                 <DropdownMenu>
                                     <DropdownMenuTrigger asChild>
                                         <SidebarMenuButton size="lg" className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
-
+                                            <User_label name={data.user.name} image={data.user.avatar} email={data.user.email} />
                                         </SidebarMenuButton>
                                     </DropdownMenuTrigger>
+                                    <DropdownMenuContent className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg mt-2"
+                                                            side={isMobile ? 'bottom' : 'left'}
+                                                            sideOffset={4}>
+                                        {/* Ajout onclick in futur */}
+                                        <DropdownMenuGroup>
+                                            <DropdownMenuItem>
+                                                <div className="flex align-middle gap-2">
+                                                    <Settings2 />
+                                                    <span className="text-sm font-semibold">Settings</span>
+                                                </div>
+                                            </DropdownMenuItem>
+                                        </DropdownMenuGroup>
+
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem>
+                                            <div className="flex align-middle gap-2">
+                                                <LogOut className="h-4 w-4 text-red-600"/>
+                                                <span className="text-sm font-semibold text-red-500">Log out</span>
+                                            </div>
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
                                 </DropdownMenu>
                             </SidebarMenuItem>
                         </SidebarMenu>
                     </SidebarMenuItem>
                 </SidebarHeader>
+                <SidebarContent className="ml-2">
+                    <SidebarGroupLabel>Todo</SidebarGroupLabel>
+                    <Collapsible
+                        asChild
+                        className="group/collapsible"
+                    >
+                        <SidebarMenuItem>
+                            <CollapsibleTrigger asChild>
+                                <SidebarMenuButton>
+                                    Test
+                                    <ChevronRight className="ml-auto transition-transform duration-300 group-data-[state=open]/collapsible:rotate-90" />
+                                </SidebarMenuButton>
+                            </CollapsibleTrigger>
+                            <CollapsibleContent>
+                                <SidebarMenuSub>
+                                    <SidebarMenuSubItem>
+                                        <SidebarMenuSubButton asChild>
+                                            <span>test</span>
+                                        </SidebarMenuSubButton>
+                                    </SidebarMenuSubItem>
+                                </SidebarMenuSub>
+                            </CollapsibleContent>
+                        </SidebarMenuItem>
+                    </Collapsible>
+                </SidebarContent>
             </Sidebar>
         </SidebarProvider>
     );
