53a32357e9cf84e4f8dd81c41f1b00ba3ebeb749
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2025-11-10 14:52:44 +0100

    save token in local storage end

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -5,4 +5,25 @@ const instance = axios.create({
     timeout: 10000
 })
 
+instance.interceptors.request.use(function (request) {
+    const token = localStorage.getItem("token")
+    if(token) {
+        request.headers["Authorization"] = `Bearer ${token}`
+    }
+    return request;
+}, function (error) {
+    return Promise.reject(error);
+});
+
+instance.interceptors.response.use(function (response) {
+    const token = response.data.token;
+    if (token) {
+        localStorage.setItem("token", token);
+    }
+
+    return response;
+}, function (error) {
+    return Promise.reject(error);
+});
+
 export default instance;
