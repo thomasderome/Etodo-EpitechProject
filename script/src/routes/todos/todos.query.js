bb1e2ef8a9829f7810788ce53a47d2edb2a81110
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2025-11-12 16:19:24 +0100

    add new route todo

--- script/src/routes/todos/todos.query.js
+++ script/src/routes/todos/todos.query.js
@@ -1,6 +1,17 @@
-const express = require("express");
-const router = express.Router();
+const { pool } = require("../../config/db.js");
 
-router.get("/todos", async(req , res ) => {
+async function displayTodos(userID) {
+    const [rows] = await pool.query("SELECT * FROM todo WHERE user_id=?", [userID])
+    return rows;
+}
 
-})
+async function viewTodo(todoID, userID){
+    const [rows] = await pool.query("SELECT * FROM todo WHERE id=? AND user_id=?", [todoID,userID])
+    return rows[0];
+}
+
+async function createTodo(userID){
+    const [rows] = await pool.query("INSERT INTO todo (title, description, due_time, user_id, status) VALUES (?, ?, ?, ?, ?)")
+}
+
+module.exports = {displayTodos, viewTodo, createTodo}
