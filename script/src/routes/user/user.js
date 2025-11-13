c31bb631c2b3d56e19fb2ca1e2eef2e9fc78eb16
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2025-11-13 16:26:08 +0100

    Fix add password in get userInformation

--- script/src/routes/user/user.js
+++ script/src/routes/user/user.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {userInformation} = require("./user.query.js")
+const {userInformation, allUserTask} = require("./user.query.js")
 
 router.get("/user", async (req, res) => {
     const user = await userInformation(req.user_id);
     res.send(user);
+})
+
+router.get("/user/todos", async (req, res) => {
+    const task = await allUserTask(req.user_id);
+    res.send(task);
 })
 
 module.exports = router;
--- script/src/routes/user/user.query.js
+++ script/src/routes/user/user.query.js
@@ -1,8 +1,12 @@
 const { pool } = require("../../config/db.js");
 
 async function userInformation(userID) {
-    const [rows] = await pool.query("SELECT id, email, created_at, firstname, name FROM user WHERE id=?", [userID]);
+    const [rows] = await pool.query("SELECT id, email, password, created_at, firstname, name FROM user WHERE id=?", [userID]);
     return rows[0];
 }
 
-module.exports = {userInformation}
+async function allUserTask(userID) {
+
+}
+
+module.exports = {userInformation, allUserTask};
