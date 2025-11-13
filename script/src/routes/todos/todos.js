9b91436d983429e356f5325d0b3bbca8290d00e4
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2025-11-13 11:47:15 +0100

    Add todo route createTodo et updateTodo

--- script/src/routes/todos/todos.js
+++ script/src/routes/todos/todos.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {displayTodos, viewTodo, createTodo} = require("./todos.query.js")
+const {displayTodos, viewTodo, createTodo, updateTodo} = require("./todos.query.js")
 
 
 router.get("/todos", async(req , res ) => {
@@ -20,11 +20,39 @@ router.get("/todos/:id", async(req , res ) => {
 })
 
 router.post("/todos", async(req, res) =>{
-    const newTodo = await createTodo()
+    const {title, description, due_time, status} = req.body;
+    if (typeof title !== "string" || typeof description !== "string" || typeof due_time !== "string"  ||typeof status !== "string" || !title || !description || !due_time || !status) {
+        throw new TypeError( );
+    }
+    const newTodo = await createTodo({
+            title: title,
+            description: description,
+            due_time: due_time,
+            user_id : req.user_id,
+            status: status
+    })
+    const todos = await displayTodos(req.user_id);
+    res.send(todos);
 })
 
 router.put("/todos/:id", async(req, res) =>{
+    const {title, description, due_time, status} = req.body;
+    const todoId = req.params.id;
+    const updatetodo = await updateTodo({
+        title: title,
+        description: description,
+        due_time: due_time,
+        user_id : req.user_id,
+        status: status,
+        todo_id: todoId
+    })
+    const todo = await viewTodo(req.params.id, req.user_id);
 
+    if (todo) {
+        res.send(todo)
+    } else {
+        res.status(403).send()
+    }
 })
 
 router.delete("/todos/:id", async(req, res) =>{
--- script/src/routes/todos/todos.query.js
+++ script/src/routes/todos/todos.query.js
@@ -10,8 +10,16 @@ async function viewTodo(todoID, userID){
     return rows[0];
 }
 
-async function createTodo(userID){
-    const [rows] = await pool.query("INSERT INTO todo (title, description, due_time, user_id, status) VALUES (?, ?, ?, ?, ?)")
+async function createTodo(data){
+    const [rows] = await pool.query("INSERT INTO todo (title, description, due_time, user_id, status) VALUES (?, ?, ?, ?, ?)", [data.title, data.description, data.due_time, data.user_id, data.status])
+    return rows[0];
 }
 
-module.exports = {displayTodos, viewTodo, createTodo}
+async function updateTodo(data){
+    const [rows] = await pool.query("UPDATE todo SET title=?, description=?, due_time=?, status =? WHERE id=? AND user_id=?",
+        [data.title, data.description, data.due_time, data.status, data.todo_id, data.user_id]);
+
+    return rows[0];
+}
+
+module.exports = {displayTodos, viewTodo, createTodo, updateTodo}
