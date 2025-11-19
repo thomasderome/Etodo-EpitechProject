622f9191c75b9aede9085676de8a363eb78802e6
Trolldu47 <mrtrolldu47@gmail.com>
2025-11-19 10:50:34 +0100

    config app

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,9 +2,12 @@
 
 
 import {Button} from "@/components/animate-ui/components/buttons/button";
+import {useRouter} from "next/router";
 
 export default function main() {
+    const router = useRouter();
+
     return (
-        <Button onClick={() => location.href = "/login"}>Login</Button>
+        <Button onClick={() => router.push("/login")}>Login</Button>
     )
 }
