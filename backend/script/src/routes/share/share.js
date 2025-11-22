df6b27cad3946905b849868cabd1600a0cde3332
Trolldu47 <mrtrolldu47@gmail.com>
2025-11-22 17:10:43 +0100

    add component route for managin share and fix sql command erreur

--- backend/script/src/routes/share/share.js
+++ backend/script/src/routes/share/share.js
@@ -1,22 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {create_share, get_share, get_user_in_share, remove_share, get_setting} = require("./share.query");
-
-// ROUTE FOR CREATE SHARE
-router.post("/", async (req, res) => {
-    const { todo_id, mode, email_share } = req.body;
-    const user_id = req.user_id;
-    if (user_id === req.user_id) res.status(403).send()
-    const response = await create_share({
-        user_id: user_id,
-        mode: mode,
-        email_share: email_share,
-        todo_id: todo_id,
-    })
-
-    if (response === null) res.status(403).send()
-    else res.status(200).send(response);
-})
+const {create_share, get_share, get_user_in_share, remove_share, get_setting, change_mode} = require("./share.query");
 
 // ROUTE FOR GET ALL TODO LIST SHARE
 router.get("/", async (req, res) => {
@@ -24,33 +8,61 @@ router.get("/", async (req, res) => {
     res.status(200).send(response);
 })
 
-router.get("/setting/:todo_list_id", async (req, res) => {
-    const { todo_list_id } = req.params;
-    const user_id = req.user_id;
-
-    const result = get_setting(user_id, todo_list_id);
-    if (result === null) res.status(403).send();
-    else res.status(200).send(result);
-})
-
 // ROUTE FOR GET ALL USER IN TODO_LIST
 router.get("/:todo_list_id", async (req, res) => {
     const response = await get_user_in_share(Number(req.params.todo_list_id), req.user_id);
 
-    if (response === null) res.status(403).send()
-    else res.status(200).send(response);
+    if (response === null) return res.status(403).send()
+    res.status(200).send(response);
+})
+
+// ALL ROUTE FOR MANAGE SHARE SYSTEM
+// GET SETTING
+router.get("/setting/:todo_list_id", async (req, res) => {
+    const todo_list_id = Number(req.params.todo_list_id);
+    const user_id = req.user_id;
+
+    if (!user_id || typeof user_id !== "string" || !todo_list_id || typeof todo_list_id !== "number") throw new TypeError();
+
+    const result = await get_setting(user_id, todo_list_id);
+
+    if (result === null) return res.status(403).send();
+    res.status(200).send(result);
+})
+
+// CHANGE MOD EDIT
+router.patch("/setting/:share_id", async (req, res) => {
+    const result = await change_mode(req.params.share_id, req.user_id);
+
+    if (result === null) return res.status(403).send();
+    res.status(204).send();
 })
 
 // DELETE SHARE FOR SPECIFIC USER
-router.delete("/", async (req, res) => {
-    const { user_remove_share, todo_list_id } = req.body;
-
+router.delete("/setting/:share_id", async (req, res) => {
     const response = await remove_share({
         user_id: req.user_id,
-        user_remove_share: user_remove_share,
-        todo_list_id: todo_list_id,
+        share_id: req.params.share_id
     });
 
+    if (response === null) return res.status(403).send();
+    res.status(204).send();
+})
+
+// ROUTE FOR CREATE SHARE
+router.post("/setting", async (req, res) => {
+    const { todo_list_id, mode, email } = req.body;
+    const user_id = req.user_id;
+
+    const response = await create_share({
+        user_id: user_id,
+        mode: mode,
+        email_share: email,
+        todo_id: todo_list_id,
+    })
+
+    if (response === null) return res.status(403).send()
+    res.status(200).send(response);
 })
 
 module.exports = router;
--- backend/script/src/routes/share/share.query.js
+++ backend/script/src/routes/share/share.query.js
@@ -1,24 +1,23 @@
 const { pool } = require("../../config/db");
 
 async function create_share(data) {
-    const [verif] = await pool.query("SELECT * FROM todo WHERE id=? AND user_id=?", [data.todo_id, data.user_id]);
+    const [verif] = await pool.query("SELECT id FROM todo WHERE id=? AND user_id=?", [data.todo_id, data.user_id]);
     if (!verif[0]) return null;
 
     const [user_share] = await pool.query("SELECT id FROM user WHERE email=?", [data.email_share]);
-
     const [verif_share_exist] = await pool.query("SELECT user_id FROM shared_todo WHERE user_id=?", [user_share[0].id]);
     if (verif_share_exist[0]) return null
 
     const [id] = await pool.query("INSERT INTO shared_todo (user_id, todo_list_id, mode) VALUES (?, ?, ?)", [user_share[0].id, data.todo_id, data.mode]);
-    const [user] = await pool.query("SELECT user.email, user.name, user.firstname FROM shared_todo JOIN user ON user.id=shared_todo.user_id WHERE shared_todo.id = ?", [id.insertId]);
+    const [user] = await pool.query("SELECT user.email, shared_todo.mode, shared_todo.id FROM shared_todo JOIN user ON user.id=shared_todo.user_id WHERE shared_todo.id = ?", [id.insertId]);
     return user[0]
 }
 
 async function get_setting(user_id, todo_list_id) {
-    const verif = pool.query("SELECT id FROM todo WHERE user_id=? AND id=?", [user_id, todo_list_id]);
+    const verif = await pool.query("SELECT id FROM todo WHERE user_id=? AND id=?", [user_id, todo_list_id]);
     if (!verif[0]) return null;
 
-    const [result] = pool.query("SELECT * FROM shared_todo WHERE todo_list_id=?", [todo_list_id]);
+    const [result] = await pool.query("SELECT shared_todo.id, shared_todo.mode, user.email FROM shared_todo JOIN user ON user.id=shared_todo.user_id WHERE shared_todo.todo_list_id=?", [todo_list_id]);
     return result
 }
 
@@ -45,12 +44,18 @@ async function get_user_in_share(todo_list_id, user_id) {
     return all_data_user;
 }
 
-async function remove_share(data) {
-    const [verif] = await pool.query("SELECT * FROM shared_todo WHERE user_id=? AND id=?", [data.user_id, data.todo_list_id]);
+async function change_mode(share_id, user_id) {
+    const [verif] = await pool.query("SELECT * FROM shared_todo JOIN todo ON todo.id=shared_todo.todo_list_id WHERE todo.user_id=? AND shared_todo.id", [user_id, share_id]);
     if (!verif[0]) return null;
 
-    await pool.query("DELETE FROM shared_todo WHERE user_id=?", [data.user_remove_share]);
-    return true;
+    await pool.query("UPDATE shared_todo SET mode=IF(mode, false, true) WHERE id=?", [share_id]);
 }
 
-module.exports = {create_share, get_share, get_user_in_share, remove_share, get_setting};
+async function remove_share(data) {
+    const [verif] = await pool.query("SELECT * FROM shared_todo JOIN todo ON todo.id=shared_todo.todo_list_id WHERE todo.user_id=? AND shared_todo.id", [data.user_id, data.share_id]);
+    if (!verif[0]) return null;
+
+    await pool.query("DELETE FROM shared_todo WHERE id = ?", [data.share_id]);
+}
+
+module.exports = {create_share, get_share, get_user_in_share, remove_share, get_setting, change_mode};
