adab06d7b80f16f59e2fefb1ddb520aac8e01f1f
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2025-11-24 12:24:54 +0100

    Add created at, due time and status in progress to task table and remove created at and due time to todo table

--- db_script/e-todo.sql
+++ db_script/e-todo.sql
@@ -12,9 +12,6 @@ CREATE TABLE user (
 CREATE TABLE todo (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title varchar(255) NOT NULL,
-    description TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    due_time DATE NOT NULL,
     status ENUM('todo', 'in progress', 'done') NOT NULL DEFAULT 'todo',
     user_id VARCHAR(36) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
@@ -24,7 +21,9 @@ CREATE TABLE task (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title varchar(255) NOT NULL,
     description TEXT NOT NULL,
-    status ENUM('todo', 'done') NOT NULL DEFAULT 'todo',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    due_time DATE NOT NULL,
+    status ENUM('todo', 'in progress', 'done') NOT NULL DEFAULT 'todo',
     todo_id INT NOT NULL,
     FOREIGN KEY (todo_id) REFERENCES todo(id) ON DELETE CASCADE
 );
