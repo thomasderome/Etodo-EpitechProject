80cc88fb6749696fb6982bf4d5c29aa2a0684b44
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2025-11-24 11:02:39 +0100

    Fix route todos, remove description/due_time/created_at

--- backend/script/src/routes/todos/todos.query.js
+++ backend/script/src/routes/todos/todos.query.js
@@ -11,13 +11,13 @@ async function viewTodo(todoID, userID){
 }
 
 async function createTodo(data){
-    const [rows] = await pool.query("INSERT INTO todo (title, description, due_time, user_id, status) VALUES (?, ?, ?, ?, ?)", [data.title, data.description, data.due_time, data.user_id, data.status])
+    const [rows] = await pool.query("INSERT INTO todo (title, user_id, status) VALUES (?, ?, ?)", [data.title, data.user_id, data.status])
     return rows;
 }
 
 async function updateTodo(data){
-    const [rows] = await pool.query("UPDATE todo SET title=?, description=?, due_time=?, status =? WHERE id=? AND user_id=?",
-        [data.title, data.description, data.due_time, data.status, data.todo_id, data.user_id]);
+    const [rows] = await pool.query("UPDATE todo SET title=?, status =? WHERE id=? AND user_id=?",
+        [data.title, data.status, data.todo_id, data.user_id]);
 
     return rows[0];
 }
