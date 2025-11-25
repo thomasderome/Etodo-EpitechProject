b80dd755e38dd533cc5b42c9d440903fa1380020
Trolldu47 <mrtrolldu47@gmail.com>
2025-11-25 18:31:26 +0100

    [MOVE] => move db script for marvine

---
+++ e-todo.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE IF NOT EXISTS etodo;
+USE etodo;
+
+CREATE TABLE user (
+    id varchar(36) PRIMARY KEY NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    name varchar(100) NOT NULL,
+    firstname varchar(100) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE todo_list (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    status ENUM('todo', 'in progress', 'done') NOT NULL DEFAULT 'todo',
+    user_id VARCHAR(36) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE todo (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    description TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    due_time DATE NOT NULL,
+    status ENUM('todo', 'in progress', 'done') NOT NULL DEFAULT 'todo',
+    todo_list_id INT NOT NULL,
+    FOREIGN KEY (todo_list_id) REFERENCES todo_list(id) ON DELETE CASCADE
+);
+
+CREATE TABLE shared_todo (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+
+    user_id VARCHAR(36) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+
+    todo_list_id INT NOT NULL,
+    FOREIGN KEY (todo_list_id) REFERENCES todo_list(id) ON DELETE CASCADE,
+
+    mode BOOL NOT NULL DEFAULT FALSE, -- IF FALSE READ ONLY ELSE WRITE AND READ
+
+    UNIQUE(user_id, todo_list_id)
+);
+
