b353523ae8a7e23cbce504de3887907237f92008
Trolldu47 <mrtrolldu47@gmail.com>
2025-11-25 18:42:39 +0100

    [FIX] => Fix command sql

--- backend/script/src/socket/socket.query.js
+++ backend/script/src/socket/socket.query.js
@@ -1,7 +1,7 @@
 const { pool } = require("../config/db");
 
 async function verif_todo_access(user_id, todo_id) {
-    let [verif] = await pool.query("SELECT id FROM todo WHERE user_id=? AND id=?", [user_id, todo_id]);
+    let [verif] = await pool.query("SELECT id FROM todo_list WHERE user_id=? AND id=?", [user_id, todo_id]);
     if (!verif[0]) [verif] = await pool.query("SELECT id FROM shared_todo WHERE user_id=? AND todo_list_id=?", [user_id, todo_id]);
     return verif[0]
 }
