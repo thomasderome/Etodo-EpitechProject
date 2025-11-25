e2ef5193189e9697c9e67477bd45ea1d89360cc2
Trolldu47 <mrtrolldu47@gmail.com>
2025-11-25 14:54:46 +0100

    [ADD] => verif if user allow to join specific todo

---
+++ backend/script/src/socket/socket.query.js
@@ -0,0 +1,9 @@
+const { pool } = require("../config/db");
+
+async function verif_todo_access(user_id, todo_id) {
+    let [verif] = await pool.query("SELECT id FROM todo WHERE user_id=? AND id=?", [user_id, todo_id]);
+    if (!verif[0]) [verif] = await pool.query("SELECT id FROM shared_todo WHERE user_id=? AND todo_list_id=?", [user_id, todo_id]);
+    return verif[0]
+}
+
+module.exports = { verif_todo_access };
