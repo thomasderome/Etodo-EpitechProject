4c89bf2f13a4fcc02c4abc3f1220aef5a8974c67
Trolldu47 <mrtrolldu47@gmail.com>
2025-11-25 17:02:28 +0100

    [PRETTIER] => format

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/animate-ui/components/buttons/button';
 import { useRouter } from 'next/router';
 
 export default function main() {
-  const router = useRouter();
+    const router = useRouter();
 
-  return <Button onClick={() => router.push('/login')}>Login</Button>;
+    return <Button onClick={() => router.push('/login')}>Login</Button>;
 }
